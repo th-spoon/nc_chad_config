12c0b151644c32396c3c694b1a0280d7e7aab27c
Wm. Weatherspoon <william_weatherspoon@intuit.com>
2024-10-14 20:43:00 -0700

    chore: add surround

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -32,5 +32,14 @@ return {
   		},
   	},
   },
-  { "kylechui/nvim-surround" }, -- For nvim-surround
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+  }
 }
